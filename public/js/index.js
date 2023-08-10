acbcaa72ef4a5293580bc509d6aca3add13ec839
JuanDiaz1992 <juannavegante2010@gmail.com>
2023-08-09 20:56:16 -0500

    create index.html file

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const traerDatos =()=>{
+    fetch("url",{
+
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        let horas = data.data;
+        for (let index = 0; index < horas.length; index++) {
+            const element = array[index];
+            
+        }
+    })
+}
